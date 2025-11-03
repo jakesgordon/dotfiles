443045759505bc49a8186f48ec58c0ccde6d4152
Jake Gordon <jakesgordon@gmail.com>
2025-11-03 07:52:18 -0800

    add prompt alias to reset my fish prompt on fresh install

--- fish/config.fish
+++ fish/config.fish
@@ -56,6 +56,7 @@ if status is-interactive
   abbr kernel   'uname -r'
   abbr fancytop 'bpytop'
   abbr ftop     'bpytop'
+  abbr prompt   "tide configure --auto --style=Lean --prompt_colors='True color' --show_time=No --lean_prompt_height='One line' --prompt_spacing=Compact --icons='Few icons' --transient=No"
 
   abbr ci        'cd ~/codeincomplete'
   abbr dot       'cd ~/.dotfiles'
